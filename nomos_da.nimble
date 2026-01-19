0e4d45d48afbc592bc9b61f09b9e8ed406a043da
aimendj <aimen.djari@gmail.com>
2026-01-19 19:11:37 +0100

    Add types module and update build configuration

--- nomos_da.nimble
+++
@@ -1,7 +0,0 @@
-version       = "0.1.0"
-authot        = "aimendj"
-description   = "Nim wrapper for nomos-da Rust library"
-srcDir        = "src"
-bin           = @["nomos_da"]
-
-requires "nim >= 2.0"
---
+++ src/kzg_nomos_da/types.nim
@@ -0,0 +1,14 @@
+type
+  NomosDaResult* = enum
+    Success = 0
+    ErrorInvalidInput = -1
+    ErrorInternal = -2
+    ErrorAllocation = -3
+
+  NomosDaError* = object
+    code*: int32
+    message*: string
+
+  EncoderHandle* = distinct pointer
+  EncodedDataHandle* = distinct pointer
+  CSizeT* = csize_t
